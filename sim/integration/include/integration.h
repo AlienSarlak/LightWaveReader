91aaf43cfb7d5d231ff4507f7254b67fa654ba9a
Ali Sarlak <ali.sarlak@protonmail.com>
2024-08-10 20:12:28 +0000

    integration first draft

---
+++ sim/integration/include/integration.h
@@ -0,0 +1,9 @@
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+
+// Function prototypes for numerical integration methods
+float trapezoidal(float arr[], int n, float a, float b);
+float midpoint(float arr[], int n, float a, float b);
+float simpsons(float arr[], int n, float a, float b);
+
+#endif // INTEGRATION_H
